3127e7b9bf457b754b0473ecbd5b9538e31616af
Jolie Bonner <2472112b@student.gla.ac.uk>
2021-02-09 23:44:49 +0000

    chapt 10

--- rango/views.py
+++ rango/views.py
@@ -1,19 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from rango.models import Category
-from rango.models import Page
-from rango.forms import CategoryForm
-from django.shortcuts import redirect
-from rango.forms import PageForm
+from rango.models import Category, Page
+from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.urls import reverse
-from rango.forms import UserForm, UserProfileForm
-from django.contrib.auth import authenticate, login
-from django.http import HttpResponse
-from django.urls import reverse
-from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
-
+from datetime import datetime
 
 def index(request):
 	# Query the database for a list of ALL categories currently stored.
@@ -27,11 +19,21 @@ def index(request):
 	context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
 	context_dict['categories'] = category_list
 	context_dict['pages'] = page_list
-	# Render the response and send it back!
-	return render(request, 'rango/index.html', context=context_dict)
+	
+	visitor_cookie_handler(request)
+
+	response = render(request, 'rango/index.html', context=context_dict)
+	return response
 	
 def about(request):
-	context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+	context_dict = {}
+	'''
+	if request.session.test_cookie_worked():
+		print("TEST COOKIE WORKED!")
+		request.session.delete_test_cookie()
+	'''
+	visitor_cookie_handler(request)
+	context_dict['visits'] = request.session['visits']
 	return render(request, 'rango/about.html', context=context_dict)
 	
 	
@@ -225,3 +227,30 @@ def user_logout(request):
 	logout(request)
 	# Take the user back to the homepage.
 	return redirect(reverse('rango:index'))
+	
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+	val = request.session.get(cookie)
+	if not val:
+		val = default_val
+	return val
+
+
+# Updated the function definition
+def visitor_cookie_handler(request):
+	visits = int(get_server_side_cookie(request, 'visits', '1'))
+	last_visit_cookie = get_server_side_cookie(request,
+	'last_visit',
+	str(datetime.now()))
+	last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+	'%Y-%m-%d %H:%M:%S')
+	# If it's been more than a day since the last visit...
+	if (datetime.now() - last_visit_time).days > 0:
+		visits = visits + 1
+		# Update the last visit cookie now that we have updated the count
+		request.session['last_visit'] = str(datetime.now())
+	else:
+		# Set the last visit cookie
+		request.session['last_visit'] = last_visit_cookie
+	# Update/set the visits cookie
+	request.session['visits'] = visits
